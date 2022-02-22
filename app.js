d8a3005adee708292ae0e596d666b3b456bbecb3
Aishahassan1 <aishahassan_5@hotmail.com>
2022-02-22 08:24:06 +0000

    add project plane spotting

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const hamburgerButton = document.querySelector("#hamburger")
-const navList = document.querySelector("ul")
+const navList = document.querySelector("#nav-list")
 
 function toggle() {
 navList.classList.toggle('show')
